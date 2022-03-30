f1cc3a8f2ba5bb30493fdaf597a68cf9dc483a04
kgiun1 <kgiun1@naver.com>
2022-03-30 13:46:20 +0900

    ♻️ refactoring #7

---
+++ part2/ch03/src/routes/index.js
@@ -0,0 +1,29 @@
+import VueRouter from "vue-router";
+import MainPage from "@/pages/MainPage.vue";
+import LoginPage from "@/pages/LoginPage.vue";
+import ProfilePage from "@/pages/ProfilePage.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: MainPage,
+  },
+  { path: '/login', component: LoginPage },
+  {
+    path: '/profile',
+    component: ProfilePage,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('token')) {
+        next();
+      } else {
+        next('/login');
+      }
+    },
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
